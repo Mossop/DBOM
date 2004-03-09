cdddfe14d3ca9e1119badc8891c18323d2a9a56c
Dave Townsend <dtownsend@oxymoronical.com>
2004-03-09 19:01:42 +0000

    *** empty log message ***

--- src/com/blueprintit/dbom/Table.java
+++ src/com/blueprintit/dbom/Table.java
@@ -1,9 +1,44 @@
 package com.blueprintit.dbom;
 
+import java.util.Set;
+
 /**
  * @author Dave
+ * 
+ * A Table represents the known information about the table in the database. The table also maintains
+ * a cache of previously retrieved records from the database. This means that when the same record is
+ * returned from two different queries, the same TableRecord will be returned.
  */
 public class Table
 {
-
+	private Database database;
+	private Set primarykey;
+	private Set fields;
+	private String name;
+	
+	public Table(Database db, String name)
+	{
+		this.name=name;
+		database=db;
+	}
+	
+	public Set getPrimaryKey()
+	{
+		return primarykey;
+	}
+	
+	public Set getFields()
+	{
+		return fields;
+	}
+	
+	public RecordSet getRecords()
+	{
+		return null;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
 }
